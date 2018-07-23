2e8e911027417c5ddfd8099ed096e6196d7ab2a6
SvtlnR <svet.ragulina@gmail.com>
2018-07-23 11:49:46 +0300

    first commit

--- src/utils/Db.php
+++ src/utils/Db.php
@@ -1,67 +1,32 @@
 <?php 
  namespace parser\utils;
 use PDO;
-// class Db{
-// 	static private $instance = null;
-// 	private function __construct(){}
-// 	public static function getInstance(){
-// 		if(!self::$instance){
-// 			try {
-// 				$host = 'localhost';
-// 				$dbname = 'parseSites';
-// 				$dbuser = 'root';
-// 				$dbpassword =''; 
-// 				$dsn='mysql:host='.$host.';dbname='.$dbname;
-// 				self::$instance=new PDO($dsn, $dbuser,$dbpassword);	
-// 				} catch (PDOException $e) {
-// 					//ErrorOutput::err_out($e->getMessage());
-// 					$instance=null;	
-// 				}
-// 			}	
-// 			return self::$instance;
-// 		}
-
-// 	}
 class Db
 {
-public static $connection;
-
-/**
- * Db constructor.
- */
-private function __construct()
-{
-}
-
-private function __wakeup()
-{
-}
-
-private function __clone()
-{
-}
-
-/**
- * @param $params
- * @return \Illuminate\Database\Capsule\Manager
- */
-public static function initConnection($params=[])
-{
-    if (self::$connection === null) {
-        $capsule = new \Illuminate\Database\Capsule\Manager;
-        $capsule->addConnection($params);
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
-        self::$connection = $capsule;
-    }
-    return self::$connection;
-}
-
-/**
- * @return mixed
- */
-public static function getConnection()
-{
-    return self::$connection;
-}
+	public static $connection;
+	private function __construct()
+	{
+	}
+	/**
+	 * @param $params
+	 * @return \Illuminate\Database\Capsule\Manager
+	 */
+	public static function initConnection($params=[])
+	{
+	    if (self::$connection === null) {
+	        $capsule = new \Illuminate\Database\Capsule\Manager;
+	        $capsule->addConnection($params);
+	        $capsule->setAsGlobal();
+	        $capsule->bootEloquent();
+	        self::$connection = $capsule;
+	    }
+	    return self::$connection;
+	}
+	/**
+	 * @return mixed
+	 */
+	public static function getConnection()
+	{
+	    return self::$connection;
+	}
 }
